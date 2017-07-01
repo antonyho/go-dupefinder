8a90071553d8468b0679a245a215bde449a12c24
Antony Ho <ntonyworkshop@gmail.com>
2017-07-01 18:32:25 +0800

    Add file sorting wrapper

--- file/models.go
+++ file/models.go
@@ -6,3 +6,36 @@ type Group struct {
 	Files    []database.File
 	Checksum string
 }
+
+type Files []*database.File
+
+func (s Files) Len() int {
+	return len(s)
+}
+func (s Files) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+type BySize struct {
+	Files
+}
+
+func (s BySize) Less(i, j int) bool {
+	return s.Files[i].Size < s.Files[j].Size
+}
+
+type ByCreationTime struct {
+	Files
+}
+
+func (s ByCreationTime) Less(i, j int) bool {
+	return s.Files[i].CreationTime.Before(s.Files[j].CreationTime)
+}
+
+type ByModificationTime struct {
+	Files
+}
+
+func (s ByModificationTime) Less(i, j int) bool {
+	return s.Files[i].ModificationTime.Before(s.Files[j].ModificationTime)
+}
