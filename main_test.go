cbd6bf304e707ff0b141cf170f664def79340186
Antony Ho <ntonyworkshop@gmail.com>
2017-07-01 19:00:41 +0800

    Test template

--- main_test.go
+++ main_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// TODO Create directories
+	// TODO Create identical files seperated in different directories
+	exitCode := m.Run()
+	// TODO Remove created files
+	// TODO Check file list
+
+	os.Exit(exitCode)
+}
+
+func BenchmarkMain(b *testing.B) {
+}
