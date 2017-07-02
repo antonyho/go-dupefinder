d7cac7c82078928ad0718a5f83961805f1aae53d
Antony Ho <ntonyworkshop@gmail.com>
2017-07-03 02:06:28 +0800

    Test template

--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"testing"
 )
 
+/*
 func TestMain(m *testing.M) {
 	// TODO Create directories
 	// TODO Create identical files seperated in different directories
@@ -12,6 +14,14 @@ func TestMain(m *testing.M) {
 	// TODO Remove created files
 	// TODO Check file list
 
+	os.Exit(exitCode)
+}
+*/
+func TestMain(m *testing.M) {
+	var dir string
+	flag.StringVar(&dir, "dir", "~", "Root directory to scan")
+	flag.Parse()
+	exitCode := m.Run()
 	os.Exit(exitCode)
 }
 
